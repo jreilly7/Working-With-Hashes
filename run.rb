78ff5b3685135711ee5c55881a8f66650ae440c4
jreilly7 <jreilly7@nd.edu>
2016-10-12 21:04:03 -0400

    Still need to get home state as default displayed value

--- run.rb
+++ run.rb
@@ -9,5 +9,33 @@ end
 
 get '/states' do
   # code!
+
+#Create states array
+@states=[]
+
+#Create states hash
+@state={id:'NY', name: 'New York'}
+
+#Add state hash to the state array
+@states << @state
+
+#Add more state hashes to the array
+@state={id:'FL', name: 'Florida'}
+@states << @state
+
+@state={id:'MI', name: 'Michigan'}
+@states << @state
+
+@state={id:'OH', name: 'Ohio'}
+@states << @state
+
+@state={id:'CT', name: 'Connecticut'}
+@states << @state
+
+#Sort array alphabetically by state's name
+@states.sort_by! {|s| s[:name]}
+
+
+
   erb :states, layout: :main
 end
